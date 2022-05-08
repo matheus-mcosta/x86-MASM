ca9ff56c0831a4627c2524521a58cf9205ce60e1
matheus-mcosta <matheus.cst@hotmail.com>
2022-05-07 21:47:01 -0300

    Final push

--- main.asm
+++ main.asm
@@ -50,17 +50,17 @@ MsgErroCreateFile	db	"Erro na criacao do arquivo.", CR, LF, 0
 MsgErroReadFile		db	"Erro na leitura do arquivo.", CR, LF, 0
 MsgErroWriteFile	db	"Erro na escrita do arquivo.", CR, LF, 0
 
-MsgPlus  			db	" + ", 0
-MsgCRLF				db	CR, LF, 0
+MsgPlus  			db	" + ", 0 ; Mensagem para soma de bytes lidos
+MsgCRLF				db	CR, LF, 0;	\n
 
-MsgSpace  			db		" ", 0
+MsgSpace  			db		" ", 0 ; mensage de space
 
-MsgBytesLidos        db	"Bytes: ", 0
+MsgBytesLidos        db	"Bytes: ", 0 ; mensagem de bytes lidos
 
-MsgSoma				db	"Soma: ", 0
+MsgSoma				db	"Soma: ", 0	; mensagem de soma
 
 MAXSTRING	equ		12
-String	db		MAXSTRING dup (?)
+String	db		MAXSTRING dup (?)	; String para imprimir
 
 sw_n	dw	0
 sw_f	db	0
@@ -78,31 +78,31 @@ sw_m	dw	0
 	lea		dx,FileName
     mov		al,0
 	
-	mov		ah,3dh
+	mov		ah,3dh			; abre arquivo
 	int		21h
 	mov		FileHandle,ax	
 	jnc		Next1
 	
-	lea		bx,MsgErroOpenFile
+	lea		bx,MsgErroOpenFile	; mensagem de erro
 	call	printf_s
 	
 	.exit	1
 
 Next1:
-    call	GetFileNameDst
+    call	GetFileNameDst	; pede nome do arquivo destino
 
 
 	lea		dx,FileNameDst
-	call	fcreate
+	call	fcreate			; cria arquivo
 	mov		FileHandleDst,bx
-	jnc		Ciclo
+	jnc		Ciclo		; se criou, continua	
 
 
 
 
-	mov		bx,FileHandle
+	mov		bx,FileHandle	; fecha arquivo
 	call	fclose
-	lea		bx, MsgErroCreateFile
+	lea		bx, MsgErroCreateFile	; mensagem de erro
 	call	printf_s
 	.exit	1
     
@@ -110,83 +110,83 @@ Next1:
 
 
 Ciclo:
-    mov		bx,FileHandle
-	mov		ah,3fh
+    mov		bx,FileHandle		; passa handle para bx
+	mov		ah,3fh				; le arquivo
 	mov		cx,1
-	lea		dx,FileBuffer
-	int		21h
+	lea		dx,FileBuffer		; passa buffer para dx
+	int		21h	
 
-	mov		dl,FileBuffer
+	mov		dl,FileBuffer	; passa buffer para dl
 	
 
 fimgetChar:
     
-	jnc		Next2 
+	jnc		Next2 	
 
-    lea		bx,MsgErroReadFile
+    lea		bx,MsgErroReadFile	; mensagem de erro
 	call	printf_s
 	
     mov		al,1
-	jmp		CloseAndFinal
+	jmp		CloseAndFinal	; fecha e finaliza
 
 
 Next2:
     
-    cmp		ax,0
+    cmp		ax,0			; se nao eh fim de arquivo
 	je		Fim
 	cmp dl, 126
 	ja		Ciclo	; se for maior que o ~ (126), ignora e vai para o ciclo novamente
 
 
 
-	mov     bx,[Somaindex]
+	mov     bx,[Somaindex]	; passa index para bx
 
-	add 	[Soma+bx],dl
-	inc		bx
-	cmp 	bx,3
-	jg 		ZeraIndex
+	add 	[Soma+bx],dl		; soma o caractere
+	inc		bx					; incrementa index
+	cmp 	bx,3				
+	jg 		ZeraIndex		; se for maior, zera
 
-	mov		[Somaindex],bx
+	mov		[Somaindex],bx		; passa index(bx) para index
 
-	jmp		DepoisZera
+	jmp		DepoisZera	
 
 
 
 ZeraIndex:
-	mov 	bx, 0
+	mov 	bx, 0			; zera index
 	mov 	[Somaindex],bx
 
 DepoisZera:
-	mov     bx, 1
-    add     NumeroBytes, bx
-	JNO      Over
-	add     NumeroBytes, bx
-	add   NumeroBytes2, 65535
+	mov     bx, 1			; passa 1 para bx
+    add     NumeroBytes, bx	; incrementa numero de bytes lidos
+	JNO      Over			; se nao eh overflow, continua
+	add     NumeroBytes, bx	; incrementa numero de bytes lidos
+	add   NumeroBytes2, 65535	; bota que leu 16bits
 
 
 Over:
+	mov 	bx, 1			
+	add     Counter, bx			; incrementa contador
+	mov 	bx, [Counter]			; passa contador para bx
+	cmp 	bx, 5			
+	jne     Over2				; se nao for 5, jump para over2
 	mov 	bx, 1
-	add     Counter, bx
-	mov 	bx, [Counter]
-	cmp 	bx, 5
-	jne     Over2
-	mov 	bx, 1
-	mov 	[Counter],bx
+	mov 	[Counter],bx		; zera contador
 
-	mov 	[Temps+6], dx
-	mov	    bx, FileHandleDst
+	mov 	[Temps+6], dx		; salva dx em temps	+6	
+	mov	    bx, FileHandleDst	; passa handler para bx
 	;mov     dl, LF
 	;call Linebreak
 
 	mov     dl, CR    ; habilitando os 2 pulava 2 linhas
-	call Linebreak
+	call Linebreak		; chama para pular de linha com CR
 
-	mov 	dx, [Temps+6]
+	mov 	dx, [Temps+6]	; passa temporario para dx
 
 Over2:
 	mov	    bx, FileHandleDst
-	call 	setChar
-	jnc 	Ciclo
+	call 	setChar				; escreve caractere
+	jnc 	Ciclo				; se nao eh carry, volta para inicio do ciclo
 
 	lea		bx, MsgErroWriteFile
 	call	printf_s
@@ -198,112 +198,103 @@ Over2:
 	
 
 
-Next3:
-
-
-    jmp Ciclo
-
-	mov		al,0
-	jmp		CloseAndFinal
-
 Fim:
-        lea		bx, MsgCRLF
+        lea		bx, MsgCRLF		; pula uma linha
 	    call	printf_s
 
-        lea     bx, MsgBytesLidos
+        lea     bx, MsgBytesLidos	; mensagem de bytes lidos
         call    printf_s
 
-		mov     ax,NumeroBytes2
+		mov     ax,NumeroBytes2		; passa numero de bytes lidos para ax
 		cmp    ax,0
 		je		Fim2
 		
-		lea		bx,String
-		call	sprintf_w
-
+		lea		bx,String			; passa string para bx
+		call	sprintf_w			; converte numero para string
 		; printf("%s", String);
 
-		lea		bx,String
-		call	printf_s
+		lea		bx,String		; passa string para bx
+		call	printf_s			; imprime string
 	
-		 lea		bx, MsgPlus
+		 lea		bx, MsgPlus	; mensagem de + para indicar que ja leu 16 bits
 	    call	printf_s
 Fim2:	
-		mov     ax,NumeroBytes
+		mov     ax,NumeroBytes	; passa numero de bytes lidos para ax
 		
 		lea		bx,String
-		call	sprintf_w
+		call	sprintf_w		; converte numero para string
 
 		; printf("%s", String);
 
 		lea		bx,String
-		call	printf_s
+		call	printf_s		; imprime string
 
-		lea		bx, MsgCRLF
+		lea		bx, MsgCRLF	; pula uma linha
 	    call	printf_s
 
-		lea		bx, MsgSoma
-	    call	printf_s
+		lea		bx, MsgSoma	
+	    call	printf_s		; imprime mensagem de soma
 
 		mov     ah, 0
-        mov		al,[Soma]
+        mov		al,[Soma]	; passa soma para al
 
-		call hex
+		call hex			; imprime resultado da primeira coluna da soma em hexdadecimal
 		
 	;	lea		bx,String
 	;	call	sprintf_w
 	;	lea		bx,String
 	;	call	printf_s
 
-		lea		bx, MsgSpace
+		lea		bx, MsgSpace	; pula um espaco
 	    call	printf_s
 
 		mov     ah, 0
         mov		al,[Soma+1]
 		
-		call hex
+		call hex		; imprime resultado da segunda coluna da soma em hexdadecimal
 
-		lea		bx, MsgSpace
-	    call	printf_s
+		lea		bx, MsgSpace	; pula um espaco
+	    call	printf_s	
 
 		mov     ah, 0
         mov		al,[Soma+2]
 		
-		call hex
+		call hex		; imprime resultado da terceira coluna da soma em hexdadecimal
 
-		lea		bx, MsgSpace
+		lea		bx, MsgSpace		; pula um espaco
 	    call	printf_s
 
 		mov     ah, 0
-        mov		al,[Soma+3]
-		call hex
+        mov		al,[Soma+3]	
+		call hex		; imprime resultado da quarta coluna da soma em hexdadecimal
 
 	
 		mov		dh,0
-		mov		dl,[Soma]
+		mov		dl,[Soma]		
 		mov     bx, FileHandleDst
-		call    writeResult
+		call    writeResult		; escreve resultado da primeira coluna da soma no arquivo
 
 		mov		dh,0
 		mov     dl,[Soma+1]
 		mov     bx, FileHandleDst
-		call    writeResult
+		call    writeResult		; escreve resultado da segunda coluna da soma no arquivo
 
 		mov		dh,0
 		mov     dl,[Soma+2]
 		mov     bx, FileHandleDst
-		call    writeResult
+		call    writeResult		; escreve resultado da terceira coluna da soma no arquivo
 	
 		mov		dh,0
 		mov     dl,[Soma+3]
 		mov     bx, FileHandleDst
-		call    writeResult
+		call    writeResult		; escreve resultado da quarta coluna da soma no arquivo
 
 
 
       
 
 
-CloseAndFinal:
+CloseAndFinal:				; fecha arquivos e finaliza
 	mov		bx,FileHandle		; Fecha o arquivo
 	mov		ah,3eh
 	int		21h
@@ -342,7 +333,7 @@ GetFileName	proc	near
 GetFileName	endp
 
 
-printf_s	proc	near
+printf_s	proc	near			; imprime string funcao do professor
 	mov		dl,[bx]
 	cmp		dl,0
 	je		ps_1
@@ -359,7 +350,7 @@ ps_1:
 	ret
 printf_s	endp
 
-sprintf_w	proc	near
+sprintf_w	proc	near		; converte numero para string funcao do professor
 
 ;void sprintf_w(char *string, WORD n) {
 	mov		sw_n,ax
@@ -431,7 +422,7 @@ sw_continua2:
 		
 sprintf_w	endp
 
-GetFileNameDst	proc	near
+GetFileNameDst	proc	near		; cria o nome do arquivo de destino com .res no final
 	;printf("Nome do arquivo destino: ");
 	lea  si, FileName
 	lea  di, FileNameDst
@@ -441,10 +432,10 @@ Lx:
     mov  [di],al           
     inc  si              
     inc  di
-    cmp byte ptr [di],0   ; Check for null terminator
-    jne Lx                 ; loop if not null
+    cmp byte ptr [di],0   ; se o caracter for nulo, sai do loop
+    jne Lx                
 
-	mov  [di],'.'
+	mov  [di],'.'		; adiciona o .res no final
 	inc  di
 	mov  [di],'r'
 	inc  di
@@ -461,7 +452,7 @@ Lx:
 	ret
 GetFileNameDst	endp
 
-gets	proc	near
+gets	proc	near		; le uma string  funcao do professor
 	push	bx
 
 	mov		ah,0ah						; L� uma linha do teclado
@@ -482,7 +473,7 @@ gets	proc	near
 gets	endp
 
 
-fopen	proc	near
+fopen	proc	near	; abre um arquivo
 	mov		al,0
 	mov		ah,3dh
 	int		21h
@@ -496,7 +487,7 @@ fopen	endp
 ;Sai:   BX -> handle do arquivo
 ;       CF -> 0, se OK
 ;--------------------------------------------------------------------
-fcreate	proc	near
+fcreate	proc	near		; cria um arquivo
 	mov		cx,0
 	mov		ah,3ch
 	int		21h
@@ -508,35 +499,21 @@ fcreate	endp
 ;Entra:	BX -> file handle
 ;Sai:	CF -> "0" se OK
 ;--------------------------------------------------------------------
-fclose	proc	near
+fclose	proc	near	; fecha um arquivo
 	mov		ah,3eh
 	int		21h
 	ret
 fclose	endp
 
-;--------------------------------------------------------------------
-;Fun��o	Le um caractere do arquivo identificado pelo HANLDE BX
-;		getChar(handle->BX)
-;Entra: BX -> file handle
-;Sai:   dl -> caractere
-;		AX -> numero de caracteres lidos
-;		CF -> "0" se leitura ok
-;--------------------------------------------------------------------
 
-		
 ;--------------------------------------------------------------------
-;Entra: BX -> file handle
-;       dl -> caractere
-;Sai:   AX -> numero de caracteres escritos
-;		CF -> "0" se escrita ok
-;--------------------------------------------------------------------
-setChar	proc	near
+setChar	proc	near	; escreve um caracter no arquivo, se for de um digito, imprime um 0 antes
 	mov	ah,0
 	mov al, dl
 	mov cx, 0
 	mov dx, 0
 
-	cmp al, 17
+	cmp al, 17		; se for um caracter de unm digito, imprime um 0 antes
 	jnl label2
 
 	mov [Temps+8],ax
@@ -558,10 +535,10 @@ setChar	proc	near
 	mov ax, [Temps+8]
 
 label2:
-	cmp ax, 0
+	cmp ax, 0		
 	je print2
 
-	mov bx, 16
+	mov bx, 16		; divide por 16 para obter o digito em hexadecimal
 	div bx
 	push dx
 	inc cx 
@@ -575,32 +552,32 @@ print2:
 
 	pop dx
 
-	cmp dx, 9
+	cmp dx, 9		; se for um numero de 1 a 9
 	jle continue2
 
-	add dx, 7
+	add dx, 7		; se for um numero de 10 a 16
 
 continue2:
-	add dx, 48
+	add dx, 48		; converte para ASCII
 	mov [Temps], cx
-	mov [Temps+2], bx
+	mov [Temps+2], bx	; salva registradores em Temps
 	
 
 
 
 	
 	mov  	bx, FileHandleDst
-	mov		ah,40h
+	mov		ah,40h		; escreve no arquivo
 	mov		cx,1
 	mov     dh, 0
 	mov		FileBuffer,dl
 	lea		dx,FileBuffer
-	int		21h
+	int		21h	
 
 
-	mov bx, [Temps+2]
+	mov bx, [Temps+2]	; devolve registradores 
 	mov cx, [Temps]
-	dec cx
+	dec cx				; decrementa contador
 	jmp print2
 exit2:
 	ret
@@ -609,7 +586,7 @@ setChar	endp
 	
  hex proc near
 
-	cmp ax,17
+	cmp ax,17			; se for um caracter de um digito, imprime um 0 antes
 	jnl divide
 
 	mov [Temps+7],ax
@@ -636,68 +613,52 @@ divide:
 	
 
   
-;initialize bx to 16 
 
-mov bx, 16
+	mov bx, 16			; divide por 16 para obter o digito em hexadecimal
   
-;divide it by 16
-;to convert it to Hexadecimal
     div bx
   
-;push it in the stack
-    push dx
+    push dx			; bota resultado na pilha
   
-;increment the count
-    inc cx
+    inc cx			; incrementa contador
   
-;set dx to 0
-    xor dx, dx
+    xor dx, dx		; zera dx
     jmp label1
 print1:
-;check if count
-;is greater than zero
-    cmp cx,0 
+
+    cmp cx,0 		; se o contador for zero
 	je exit
   
-;pop the top of stack
-    pop dx
+
+    pop dx			; retira resultado da pilha
   
-;compare the value
-;with 9 
-cmp dx, 9 
-jle continue
+
+	cmp dx, 9 			; se for um numero de 1 a 9
+	jle continue
   
-;if value is greater than 9
-;then add 7 so that after
-;adding 48 it represents A
-;for example 10 + 7 + 48 = 65
-;which is ASCII value of A
-    add dx, 7
+
+    add dx, 7		; se for um numero de 10 a 16
   
 continue:
   
-;add 48 so that it
-;represents the ASCII
-;value of digits
-    add dx,48
+    add dx,48			; converte para ASCII
   
-;interrupt to print a;
 
-    mov ah,02h
-	int 21h
+
+    mov ah,02h		; imprime				
+	int 21h				
 	
 
 
   
-;decrease the count
-    dec cx
+    dec cx		; decrementa contador
     jmp print1
  exit : 
  ret
 hex endp
 
 
-Linebreak	proc	near
+Linebreak	proc	near		; imprime uma linha no arquivo
 	mov		ah,40h
 	mov		cx,1
 	mov		FileBuffer,dl
@@ -716,31 +677,31 @@ mov	ah,0
 
 label3:
 	
-	cmp ax, 0
+	cmp ax, 0		
 	je print3
 
-	mov bx, 16
+	mov bx, 16		; divide por 16 para obter o digito em hexadecimal
 	div bx
-	push dx
-	inc cx 
+	push dx		; bota resultado na pilha
+	inc cx 		; incrementa contador
 
-	mov dx, 0
+	mov dx, 0	; zera dx
 	jmp label3
 
 print3:
-	cmp cx, 0
+	cmp cx, 0	; se o contador for zero
 	je exit3
 
-	pop dx
+	pop dx		; retira resultado da pilha
 
-	cmp dx, 9
+	cmp dx, 9		; se for um numero de 1 a 9
 	jle continue3
 
-	add dx, 7
+	add dx, 7		; se for um numero de 10 a 16
 
 continue3:
-	add dx, 48
-	mov [Temps], cx
+	add dx, 48		; converte para ASCII
+	mov [Temps], cx		; retorna registradores
 	mov [Temps+2], bx
 	mov [Temps+3],dx
 
@@ -767,9 +728,6 @@ continue3:
 proximasoma:
 	
 ; print  0 antes do caracter do resultado da soma
-		
-	
-	
 
 	mov dl, 30h
 	mov  	bx, FileHandleDst
@@ -799,9 +757,6 @@ proximasoma:
 
 proximasoma2:
 	
-
-	
-	
 	mov  dx, [Temps+3]
 	; print um caracter do resultado da soma
 	mov  	bx, FileHandleDst
@@ -812,16 +767,15 @@ proximasoma2:
 	lea		dx,FileBuffer
 	int		21h
 
-
 	mov bx, [Temps+2]
-	mov cx, [Temps]
+	mov cx, [Temps]			; retorna registradores
 	dec cx
 	jmp print3
 exit3:
 	ret
 writeResult	endp	
 ;--------------------------------------------------------------------
-		end
+		end		; fim do programa			07/05/2022
 ;--------------------------------------------------------------------
 
 
